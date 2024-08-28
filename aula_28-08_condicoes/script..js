9bc2bee71d98f6e3d625967bbf7cbece31bb6ffa
Antonio Ribeiro <antoniopibeironeto@gmail.com>
2024-08-28 20:07:32 -0300

    Initial commit

---
+++ aula_28-08_condicoes/script..js
@@ -0,0 +1,26 @@
+/*
+
+var palavraVariavel = ?
+
+FUÇÕES
+prompt(""):string
+parseInter("""):number
+parseFloat(""):number
+console.log(""):void
+
++ soma ou concatena
+- subtrai
+* multiplica
+/ divide
+
+CONDIÇÕES
+IF(var ou texto/number ___ var ou texto/number)
+== igual/comparação
+> maior que
+< menor que
+> maior igual
+< menor igual
+!= diferente
+
+*/
+
