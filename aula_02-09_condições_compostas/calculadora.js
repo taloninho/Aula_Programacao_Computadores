a0ffa8689e4a724e780d0e56ceb269586625c3fa
Antonio Ribeiro <antoniopibeironeto@gmail.com>
2024-09-02 22:23:34 -0300

    aula 2-9

---
+++ aula_02-09_condições_compostas/calculadora.js
@@ -0,0 +1,19 @@
+var numeroUm = parseInt(prompt("Digite o primeiro número:"));
+var numeroDois = parseInt(prompt("Digite o segundo número:"));
+var operacao = prompt("Digite cursivamente o tipo de sua operação:");
+var soma = numeroUm + numeroDois;
+var subtracao = numeroUm - numeroDois;
+var divisao = numeroUm / numeroDois;
+var multiplicacao = numeroUm * numeroDois;
+
+if (soma == soma){
+    console.log("O resultado de sua soma é:" + soma);
+} else if (subtracao == subtracao){
+    console.log("O resultado de sua subtração é" + subtracao);
+} else if (divisao == divisao){
+    console.log("O resultado de sua divisão é" + divisao);
+} else if (multiplicacao == multiplicacao){
+    console.log("O resultado de sua multiplicação é:" + multiplicacao);
+}
+
+// resolver na proxima aula
